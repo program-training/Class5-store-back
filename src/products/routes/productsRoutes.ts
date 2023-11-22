2a2cc1c5630dd22a2388dda0a9414909c5df2d0a
DavidRakov <141727866+DavidRakov@users.noreply.github.com>
2023-11-22 20:41:45 +0200

    נקודת גישה לקבל 2 מערכים, אחד עם מה קיים במלאי, השני מה חסר (מחזיר כמה חסר)

--- src/products/routes/productsRoutes.ts
+++ src/products/routes/productsRoutes.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import {
+  handleGetFromStock,
   handleGetProduct,
   handleGetProducts,
 } from "../controllers/productsControllers";
 const router = express.Router();
 
 router.get("/", handleGetProducts);
-
 router.get("/:id", handleGetProduct);
+router.post("/stock", handleGetFromStock);
 
 export default router;
--- src/products/services/productsApiService.ts
+++ src/products/services/productsApiService.ts
@@ -2,6 +2,7 @@ import {
   getProductByIdFromJsonFile,
   getProductsFromJsonFile,
 } from "../dal/productsDal";
+import { CheckQuantity, NotInStock } from "./types";
 
 export const getProducts = async () => {
   try {
@@ -17,6 +18,36 @@ export const getProduct = async (productId: number) => {
     const getProductFromMDB = await getProductByIdFromJsonFile(productId);
     // console.log(getProductFromMDB);
     return getProductFromMDB;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const getDataForQuantity = async (cart: CheckQuantity[]) => {
+  try {
+    const inStock: CheckQuantity[] = [];
+    const notInStock: NotInStock[] = [];
+    await Promise.all(
+      cart.map(async (item) => {
+        const product = await getProductByIdFromJsonFile(item.productId);
+        if (product.quantity === 0) {
+          notInStock.push({ product, amountMissing: item.amount });
+        } else if (product.quantity !== 0) {
+          const referents = item.amount - product.quantity;
+          if (referents < 0) {
+            inStock.push(item);
+          } else if (referents > 0) {
+            inStock.push({
+              productId: item.productId,
+              amount: product.quantity,
+            });
+            notInStock.push({ product, amountMissing: referents });
+          }
+        }
+      })
+    );
+
+    return { inStock, notInStock };
   } catch (error) {
     return Promise.reject(error);
   }
