f1d9bfa8a8ad4ecd76b013662db56e06871b2817
Zalman-Philip <z0533147971@gmail.com>
2023-11-22 12:44:16 +0200

    users controllers joi routes

--- src/users/models/joi/userValidation.ts
+++ src/users/models/joi/userValidation.ts
@@ -10,15 +10,6 @@ const userValidation = (user: UserInterface) => {
       )
       .rule({ message: 'user "mail" mast be a valid mail' })
       .required(),
-    password: Joi.string()
-      .ruleset.regex(
-        /((?=.*\d{1})(?=.*[A-Z]{1})(?=.*[a-z]{1})(?=.*[!@#$%^&*-]{1}).{7,20})/
-      )
-      .rule({
-        message:
-          'user "password" must be at least nine characters long and contain an uppercase letter, a lowercase letter, a number and one of the following characters !@#$%^&*-',
-      })
-      .required(),
   });
   return schema.validate(user);
 };
