44c43b23219e7b479dba85575d900ce865bf7afc
Zalman-Philip <z0533147971@gmail.com>
2023-11-22 21:14:48 +0200

    mongo db

--- src/dataAccess/mongoose.ts
+++ src/dataAccess/mongoose.ts
@@ -3,9 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connectToDatabase = async () => {
-  const connect = await mongoose.connect(
-    "mongodb+srv://admin:admin@cluster0.i8qiubt.mongodb.net/?retryWrites=true&w=majority"
-  );
-  if (connect) return "Connected to MongoDB";
-  else return "no connection to mongo :((";
+  try {
+    await mongoose.connect(
+      process.env.MONGO_URI || ""
+    );
+    return "Connected to MongoDB";
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    return "no connection to mongo :((";
+  } 
 };
