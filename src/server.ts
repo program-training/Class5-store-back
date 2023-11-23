3a0427392acc9cc78674e1893dd9b46859b8a140
Zalman-Philip <z0533147971@gmail.com>
2023-11-23 09:45:28 +0200

    PORT

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use(cors);
 app.use(express.json());
 app.use(router);
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(chalk.blueBright(`Server listening on port: ${PORT}`));
   connectToDatabase()
