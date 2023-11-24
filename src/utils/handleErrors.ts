08025075f3ca3abaaff3f9d1622369d108dcdd44
Zalman-Philip <z0533147971@gmail.com>
2023-11-24 13:43:32 +0200

    changes

--- src/utils/handleErrors.ts
+++ src/utils/handleErrors.ts
@@ -1,5 +1,4 @@
 import { Response } from "express";
-import chalk from "chalk";
 
 export const handleError = (
   res: Response,
@@ -9,10 +8,4 @@ export const handleError = (
   if (error && error instanceof Error)
     return res.status(status).send(error.message);
   return res.status(status).send("Oops... an error accorded");
-};
-
-export const handleJsonfileError = <T>(error: T) => {
-  if (error instanceof Error) return Promise.reject(error);
-  console.log(chalk.redBright(error));
-  return Promise.reject(new Error("Something went wong!"));
 };
