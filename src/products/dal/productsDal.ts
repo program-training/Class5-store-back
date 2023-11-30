cb2d31c712e541150736ce5c8c2855be562a752c
DavidRakov <141727866+DavidRakov@users.noreply.github.com>
2023-11-30 16:07:55 +0200

    eee

--- src/products/dal/productsDal.ts
+++ src/products/dal/productsDal.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { CheckQuantity } from "../types/types";
 
 const ERP_BASE_URL =
-  process.env.ERP_BASE_URL || "https://erp-server-v2.onrender.com/api";
+  process.env.ERP_BASE_URL || "https://erp-server-v2.onrender.com";
 
 export const getProductsFromDB = async () => {
   try {
