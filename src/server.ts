bb8cc14ba9df81bb5f40b7b697d0c030f706618c
daniel8298 <daniel8298@gmail.com>
2023-12-07 20:44:20 +0200

    update

--- src/server.ts
+++ src/server.ts
@@ -25,10 +25,11 @@
 
 import { connectToDatabase } from "./dataAccess/mongoose";
 import { connectedToOMS } from "./Orders/dal/orderDal";
-import { connectedToERP } from "./products/dal/productsDal";
+
 import { startStandaloneServer } from "@apollo/server/standalone";
 import server from "./graphql/apolloServer";
 import chalk from "chalk";
+import { connectedToERP } from "./products_/dal/productsDal";
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
