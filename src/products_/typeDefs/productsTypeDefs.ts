484f55135e7861ba845f8a99afbe4a74a29ece5a
daniel8298 <daniel8298@gmail.com>
2023-12-07 12:46:49 +0200

    update

---
+++ src/products_/typeDefs/productsTypeDefs.ts
@@ -0,0 +1,58 @@
+export const productTypesDefs = `
+  type Product {
+    id: Int
+    name: String
+    salePrice: String
+    quantity: Int
+    description: String
+    category: String
+    discountPercentage: Int
+    imageUrl: String
+    imageAlt: String
+
+  }
+  input CheckQuantity {
+    productId: Int
+    requiredQuantity: Int
+  }
+  input ProductInput {
+    id: Int
+    name: String
+    salePrice: String
+    quantity: Int
+    description: String
+    category: String
+    discountPercentage: Int
+    imageUrl: String
+    imageAlt: String
+  
+  }
+  input ProductToCheck {
+    productId: Int
+    requiredQuantity: Int
+  }
+  type InStock {
+    productId: Int
+    requiredQuantity: Int
+  }
+  type NotInStock {
+    product: Product
+    requiredQuantity: Int
+  }
+  type Response {
+    inStock: [InStock]!
+    notInStock: [NotInStock]!
+  }
+`;
+
+export const productsTypeDefsQueries = `
+  getProducts: [Product]
+  getProduct(id:ID): Product
+
+`;
+
+export const productsTypeDefsMutation = `
+  checkProductsInStock(cart:[ProductToCheck!]): Response
+  cancelProductsInStock(cart:[CheckQuantity!]): String!
+
+`;
