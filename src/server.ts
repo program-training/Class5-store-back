be24d2e521c934168eb5aa0c82b8f3bbf669b682
menashsoffer <menashesoffer8916@gmail.com>
2023-12-10 13:13:41 +0200

    change server to express

--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,37 @@
-// import express from "express";
-// import router from "./router/router";
-// import chalk from "chalk";
-// import morgan from "./logger/morgan";
-// import cors from "./cors/cors";
-// import handleErrorMiddleware from "./middlewares/handleErrorMiddleware";
-// const app = express();
-
-// app.use(morgan);
-// app.use(cors);
-// app.use(express.json());
-// app.use(router);
-// app.use(handleErrorMiddleware);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, async () => {
-//   console.log(chalk.blueBright(`Server listening on port: ${PORT}`));
-//   await connectedToOMS();
-//   await connectedToERP();
-//   connectToDatabase()
-//     .then((message) => console.log(message))
-//     .catch((error) => console.log(error.message));
-// });
-// export default app;
+import express from "express";
+import chalk from "chalk";
+import cors from "./cors/cors";
+import handleErrorMiddleware from "./middlewares/handleErrorMiddleware";
+import { expressMiddleware } from "@apollo/server/express4";
 
 import { connectToDatabase } from "./dataAccess/mongoose";
 import { connectedToOMS } from "./Orders/dal/orderDal";
 import { connectedToERP } from "./products/dal/productsDal";
-import { startStandaloneServer } from "@apollo/server/standalone";
 import server from "./graphql/apolloServer";
-import chalk from "chalk";
 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-})
-  .then(({ url }) => {
-    console.log(chalk.blueBright(`server run on: ${url}`));
-    connectToDatabase()
-      .then(async (message) => {
-        await connectedToOMS();
-        await connectedToERP();
-        console.log(chalk.magentaBright(message));
-      })
-      .catch((error) =>
-        console.log(
-          chalk.redBright("Connect to mongoDB Error: ", error.message)
-        )
-      );
-  })
-  .catch((error) => console.log(error.message));
+const app = express();
+
+app.use(cors);
+app.use(handleErrorMiddleware);
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, async () => {
+  console.log(chalk.blueBright(`Server listening on port: ${PORT}`));
+  connectToDatabase()
+    .then(async (message) => {
+      await connectedToOMS();
+      await connectedToERP();
+      console.log(chalk.magentaBright(message));
+    })
+    .then(async () => {
+      await server.start().then(() => {
+        app.use("/graphql", cors, expressMiddleware(server));
+        app.listen(5000);
+        console.log(chalk.bgGreen(`http://localhost:5000/graphql`));
+      });
+    })
+    .catch((error) =>
+      console.log(chalk.redBright("Connect to mongoDB Error: ", error.message))
+    );
+});
