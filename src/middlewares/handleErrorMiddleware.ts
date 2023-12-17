3130c82a81b20ac80fa4ed89939260dc3405779e
8AGaming <ap770ai@gmail.com>
2023-12-17 14:09:38 +0200

    Fix

--- src/middlewares/handleErrorMiddleware.ts
+++ src/middlewares/handleErrorMiddleware.ts
@@ -1,17 +1,18 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import ServerError from "../utils/ServerError";
 import { ErrorType } from "../types/types";
 
 const handleErrorMiddleware = (
   error: ErrorType,
   req: Request,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
   const status = error instanceof ServerError ? error.status : 500;
   const message =
     error instanceof ServerError || error instanceof Error
       ? error.message
-      : "Internal Server Error";
+      : next`Internal Server Error`;
 
   res.status(status).send(message);
 };
