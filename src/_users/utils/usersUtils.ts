d9ce49806fc526fc220e75e803d463289ee7d935
daniel8298 <daniel8298@gmail.com>
2023-12-21 15:09:10 +0200

    update

--- src/_users/utils/usersUtils.ts
+++ src/_users/utils/usersUtils.ts
@@ -5,16 +5,3 @@ export const convertUserForSending = (user: UserResInterface) => {
   const id = _id.toString();
   return { _id: id, isAdmin, email, loginCount };
 };
-
-// const pubsub = new PubSub();
-
-// export const convertUserForSending = (user: UserResInterface) => {
-//   const { _id, isAdmin, email } = user;
-//   const id = _id.toString();
-//   const userRegistered = { _id: id, isAdmin, email };
-//   pubsub.publish("USER_REGISTER", {
-//     userRegister: { ...userRegistered, id: userRegistered._id },
-//   });
-
-//   return userRegistered;
-// };
